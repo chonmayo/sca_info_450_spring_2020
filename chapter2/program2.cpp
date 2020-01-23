b0a4918a6eba7f8dcd8792921f6487034e5815d6
Shawn Armao <48003442+chonmayo@users.noreply.github.com>
2020-01-23 05:50:59 -0500

    Add files via upload

---
+++ chapter2/program2.cpp
@@ -0,0 +1,11 @@
+// Program #2 - Using a variable 
+#include <iostream>
+using namespace std;
+int main() {
+
+int x; // this declares a variable 
+x = 1023; // this assigns 1023 to x 
+std::cout << "This program prints the value of x: "; 
+std::cout << x << std::endl; // This displays 1023 return 0;
+
+}
